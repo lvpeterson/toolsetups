7b96a27025d600b7cb6645afe3f9b60862ea0b7b
Logan Peterson <lv.peterson@yahoo.com>
2017-11-27 20:23:16 -0800

    updates

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Tool Setups
+apt-get update
+
+# Lists:
+git clone https://github.com/govolution/betterdefaultpasslist.git /opt/DefaultPassLists
+git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
+
+# Apps:
+apt-get install ftp
+apt-get install nfs-common
