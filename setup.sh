b74b4a1abf8ffc32c2e7cce29dad58ae64837033
Logan Peterson <lv.peterson@yahoo.com>
2017-12-01 17:20:18 -0800

    update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ apt-get update
 git clone https://github.com/govolution/betterdefaultpasslist.git /opt/DefaultPassLists
 git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
 
+# Tools:
+git clone https://github.com/gojhonny/CredCrack /opt/CredCrack
+git clone https://github.com/leonteale/pentestpackage/blob/master/Gpprefdecrypt.py /opt/Scripts/Gpprefdecrypt.py
+git clone https://github.com/jasonjfrank/gnmap-parser /opt/gnmap-parser
+
 # Apps:
 apt-get install ftp
 apt-get install nfs-common
+apt-get install cifs-tools
